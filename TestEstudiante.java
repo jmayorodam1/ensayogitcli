6c024038b8f436bf61a7402188f868cf466219ba
JavierMayor <jmayoroyardam1>
2021-12-01 18:35:19 +0100

    Añadido estudiante

--- TestEstudiante.java
+++ TestEstudiante.java
@@ -18,6 +18,8 @@ public class TestEstudiante
         e2.printEstudiante();
         Estudiante e3 = new Estudiante("Elisa", 26);
         e3.printEstudiante();
+        Estudiante e4 = new Estudiante("Javier", 19);
+        e4.printEstudiante();
         
 
     }
